a8cca597323f2cbd3c85eaee28e54cc062ab8efa
Dave White <david@dvdwhite.com>
2016-12-04 07:26:40 -0700

    login name change

--- page-database.php
+++ page-database.php
@@ -24,7 +24,7 @@ get_header(); ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php get_template_part( 'content', 'user-login' ); ?>
+					<?php get_template_part( 'content', 'database' ); ?>
 
 					<?php
 						// If comments are open or we have at least one comment, load up the comment template
