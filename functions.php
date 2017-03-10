d51482812473f1e406390a19b9223f452a45053e
Dave White <david@dvdwhite.com>
2017-03-10 11:16:03 -0700

    modified SQL query

--- functions.php
+++ functions.php
@@ -727,7 +727,7 @@ add_filter( 'getarchives_where', 'wse95776_archives_by_cat', 10, 2 );
  * @return string
  */
 function wse95776_archives_by_cat( $where, $r ){
-    return "WHERE wp_posts.post_type = 'post' AND wp_posts.post_status = 'publish' AND wp_terms.slug = 'tribal-connections' AND wp_term_taxonomy.taxonomy = 'category'";
+    return "WHERE wp_tribal_posts.post_type = 'post' AND wp_tribal_posts.post_status = 'publish' AND wp_tribal_terms.slug = 'tribal-connections' AND wp_tribal_term_taxonomy.taxonomy = 'category'";
 }
 
 add_filter( 'getarchives_join', 'wse95776_archives_join', 10, 2 );
@@ -740,7 +740,7 @@ add_filter( 'getarchives_join', 'wse95776_archives_join', 10, 2 );
  * @return string
  */
 function wse95776_archives_join( $join, $r ){
-    return 'inner join wp_term_relationships on wp_posts.ID = wp_term_relationships.object_id inner join wp_term_taxonomy on wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id inner join wp_terms on wp_term_taxonomy.term_id = wp_terms.term_id';
+    return 'inner join wp_tribal_term_relationships on wp_tribal_posts.ID = wp_tribal_term_relationships.object_id inner join wp_tribal_term_taxonomy on wp_tribal_term_relationships.term_taxonomy_id = wp_tribal_term_taxonomy.term_taxonomy_id inner join wp_tribal_terms on wp_tribal_term_taxonomy.term_id = wp_tribal_terms.term_id';
 }
 
 
